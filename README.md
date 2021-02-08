c252ccea823e04632412bf3f8baa6d6ec6135162
SebTota <kidokyo1@gmail.com>
2021-02-08 14:10:57 -0500

    uploaded files

---
+++ README.md
@@ -0,0 +1,116 @@
+# Node.js - Google Resumable Upload
+Allows users to upload files or any size to various Google services using the 
+resumable upload API.
+
+## Usage
+```bash
+git clone https://github.com/SebTota/Node-Google-Resumable-Upload
+npm install
+```
+
+### Tokens
+This application does not get the required Google OAuth tokens, however 
+the token can be refreshed if the `refreshToken` value is set to `true`
+and the `clientId` and `clientSecret` values are set.
+
+### Uploading A Single File (Google Drive)
+```javascript
+const UploadFile = require('pathToRepo/index.js')
+let uploadFile = new UploadFile();
+uploadFile.tokens = token; // OAuth token
+uploadFile.filePath = 'path/To/File';
+uploadFile.metadataBody = {
+    name: 'file name',
+    parents: ['parentsFolderId']
+}
+uploadFile.refreshToken = true; // Optional: Refreshed expired access token
+uploadFile.clientId = ''; // Required if refreshToken = true. 
+uploadFile.clientSecret = ''; // Required if refreshToken = true. 
+uploadFile.upload(); // Initalize upload
+
+uploadFile.on('error', (e) => {
+    console.log(e)
+})
+    
+/*
+* Tracks progress of file upload. Based on chunk size, not time.
+ */
+uploadFile.on('progress', (p) => {
+    console.log(p)
+})
+
+/*
+* Returns the body of the final upload.
+ */
+uploadFile.on('success', (s) => {
+    console.log(s)
+    
+    /*
+    * Ex return when uploading image file to Google Drive
+     */
+    s = {
+        kind: 'drive#file',
+        id: 'abc123',
+        name: 'file name',
+        mimeType: 'image/jpeg'
+    }
+})
+```
+
+### Uploading Multiple Files (Google Drive)
+```javascript
+const UploadFile = require('pathToRepo/index.js')
+let files = ['/Users/user1/path/to/file1', '/Users/user1/path/to/file2']
+
+files.forEach((file) => {
+    let uploadFile = new UploadFile();
+    uploadFile.tokens = token; // OAuth token
+    uploadFile.metadataBody = {
+        name: file.split('/').pop() 
+    }
+    uploadFile.refreshToken = true;
+    uploadFile.clientId = '...';
+    uploadFile.clientSecret = '...'
+    uploadFile.filePath = file
+    uploadFile.upload();
+
+    uploadFile.on('error', (e) => {
+        console.log(`File ${file} error: ${e}`)
+    })
+
+    uploadFile.on('progress', (p) => {
+        console.log(`File ${file} progress: ${p}`)
+    })
+
+    uploadFile.on('success', (s) => {
+        console.log(s)
+    })
+})
+```
+
+### Available Parameters
+```javascript
+sentBytes = 0;
+host	= 'www.googleapis.com'; // Should rarely change
+tokens = {}; // OAuth Token
+filePath = ''; // Local path of file
+metadataBody = {}; // Dictionary containing file metadata
+apiService = '/upload/drive/v3/files'; // Indicate which Google Service to use
+retry = 0; // Number of retries when upload fails
+refreshToken = false; // Indicate if the application should try to refresh the access token if it expired
+clientId = ''; // Required if refreshToken = true
+clientSecret = ''; // Require if refreshToken = true
+chunkSize = 1024 * 1024 * 8; // Default chunk size: 8MB (estimate of 8MiB as recommended by Google)
+this.optionalParams = {}; // Additional optional url params
+```
+
+## Attribution
+This project was inspired by [node-youtube-resumable-upload](https://github.com/grayleonard/node-youtube-resumable-upload).
+
+```
+Copyright (c) 2014, Luther Blissett
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+```
---
+++ index.js
@@ -0,0 +1,164 @@
+const got           = require('got');
+const fs            = require('fs')
+const mime          = require('mime')
+const EventEmitter	= require('events').EventEmitter;
+const util          = require('util')
+
+function UploadFile() {
+    this.sentBytes = 0;
+    this.host	= 'www.googleapis.com';
+    this.tokens = {};
+    this.filePath = '';
+    this.metadataBody = {};
+    this.apiService = '/upload/drive/v3/files';
+    this.retry = 0;
+    this.refreshToken = false;
+    this.clientId = '';
+    this.clientSecret = '';
+    this.chunkSize = 1024 * 1024 * 8; // Default chunk size: 8MB (estimate of 8MiB as recommended by Google)
+    this.optionalParams = {};
+}
+
+util.inherits(UploadFile, EventEmitter);
+
+/*
+* Serialize an dictionary into a URL encoded string. The return string will be preceded with an '&'.
+* @params   {dictionary}    obj A dictionary of url parameters and values
+* @return   {string}            A URL encoded string starting with '&' if there are any key value pairs or an empty string if not
+ */
+function serializeUrl(obj) {
+    let str = [];
+    for(let p in obj)
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    if (str.length > 0)
+        return `&${str.join("&")}`;
+    return ''
+}
+
+/*
+* Refresh Google OAuth Access Token using Refresh Token
+ */
+UploadFile.prototype.refreshAccessToken = async function() {
+    const refreshUrl = `https://www.googleapis.com/oauth2/v4/token?client_id=${this.clientId}&client_secret=${this.clientSecret}&refresh_token=${this.tokens.refresh_token}&grant_type=refresh_token`;
+    try {
+        const res = await got.post(refreshUrl);
+        // Update refreshed access_token
+        this.emit('progress', 'Updating access token');
+        if (res.hasOwnProperty('body') && JSON.parse(res.body).hasOwnProperty('access_token')) {
+            this.tokens.access_token = JSON.parse(res.body)['access_token'];
+        }
+        this.emit('progress', `New access token: ${this.tokens.access_token}`)
+    } catch(e) {
+        this.emit('error', new Error(e))
+    }
+}
+
+UploadFile.prototype.upload = async function() {
+    this.fileSize = fs.statSync(this.filePath).size;
+    var options = {
+        url:	`https://${this.host}${this.apiService}?uploadType=resumable&part=snippet,status,contentDetails${serializeUrl(this.optionalParams)}`,
+        headers: {
+            'Host'                      :   this.host,
+            'Authorization'             :   'Bearer ' + this.tokens.access_token,
+            'Content-Length'            :   new Buffer.from(JSON.stringify(this.metadataBody)).length,
+            'Content-Type'              :   'application/json; charset=UTF-8',
+            'X-Upload-Content-Length'   :   this.fileSize,
+            'X-Upload-Content-Type'     : 	mime.getType(this.filePath)
+        },
+        body: JSON.stringify(this.metadataBody)
+    };
+
+    let res;
+    let err;
+    try {
+        res = await got.post(options)
+    } catch(e) {
+        if (e.response.statusCode === 401 && this.refreshToken === true) {
+            this.refreshToken = false; // Only try to refresh the token once
+            await this.refreshAccessToken(); // Refresh access token
+            await this.upload(); // Retry upload
+            return;
+        } else {
+            this.emit('error', new Error(e))
+        }
+    }
+
+    /*
+    * Retry upload depending on retry counter or continue to upload file if no error
+     */
+    if(err || !res.headers.location) {
+        if ((this.retry > 0) || (this.retry <= -1)) {
+            this.retry--;
+            await this.upload(); // Retry initiating upload
+        }
+    } else {
+        this.location = res.headers.location;
+        await this.send();
+    }
+}
+
+UploadFile.prototype.send = async function() {
+    while(this.sentBytes < this.fileSize) {
+        let startByte = this.sentBytes;
+        let bytesToSend = this.chunkSize;
+
+        // Check to make sure the number of bytes to send is less than or equal to the number of bytes left
+        if (this.fileSize - this.sentBytes < bytesToSend) {
+            // Not enough bytes to send an entire chunk.
+            // Set size to the amount of bytes left to send
+            bytesToSend = this.fileSize - this.sentBytes;
+        }
+        let endByte = startByte + bytesToSend;
+
+        let uploadPipe = fs.createReadStream(this.filePath, {
+            start: startByte,
+            end: endByte
+        });
+
+        let options = {
+            url: this.location,
+            headers: {
+                'Authorization' :   'Bearer ' + this.tokens.access_token,
+                'Content-Length':	bytesToSend,
+                'Content-Type'  :	mime.getType(this.filePath),
+                'Content-Range' :   `bytes ${startByte}-${endByte-1}/${this.fileSize}`
+            },
+            body: uploadPipe
+        };
+
+        let res;
+        try {
+            res = await got.put(options);
+        } catch (e) {
+            /*
+             * Check if error being thrown is due to status code 308
+             * Status code 308 is an expected code in this application asking the user to resume the upload
+             */
+            if (e.response.statusCode === 308) {
+                res = e
+            } else {
+                this.emit('error', new Error(e))
+            }
+        }
+
+        if (res.hasOwnProperty('body')) {
+            this.emit('success', JSON.parse(res.body))
+            break;
+        }
+
+        if (res.response.statusCode === 308 && res.response.headers.hasOwnProperty('range')) {
+            this.sentBytes = Number(res.response.headers.range.split('-').pop());
+            this.emit('progress', `Bytes sent: ${this.sentBytes}`)
+        } else {
+            if ((this.retry > 0) || (this.retry <= -1)) {
+                this.retry--;
+                this.emit('progress', 'Retrying')
+                await this.send(); // Retry initiating upload
+            } else {
+                this.emit('error', "Exiting upload")
+            }
+        }
+    }
+}
+
+module.exports = UploadFile;
